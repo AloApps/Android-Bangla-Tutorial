683b26a01f487405ec61cebeb66baaeea6f03fdf
AloApps <alorweb@gmail.com>
2016-04-01 07:51:49 -0700

    Update Shake.java

--- Shake.java
+++ Shake.java
@@ -31,25 +31,26 @@ public class Shake extends Activity implements View.OnClickListener {
 shake.xml
 <?xml version="1.0" encoding="utf-8"?>
 <LinearLayout>
-
     <EditText android:id="@+id/pw" />
-
     <Button android:id="@+id/login" />
-
 </LinearLayout>
+
+
 
 anim/shake.xml
 <?xml version="1.0" encoding="utf-8"?>
-
 <translate
         xmlns:android="http://schemas.android.com/apk/res/android"
         android:fromXDelta="0"
         android:toXDelta="10"
         android:duration="1000"
         android:interpolator="@anim/cycle_7" />
+        
+        
+        
+        
 anim/cycle_7.xml
 <?xml version="1.0" encoding="utf-8"?>
-
 <cycleInterpolator
         xmlns:android="http://schemas.android.com/apk/res/android"
         android:cycles="7" />
