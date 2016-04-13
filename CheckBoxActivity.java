65d48423bfe105480b619caa79968147029818d0
AloApps <alorweb@gmail.com>
2016-04-14 00:37:43 +0600

    Create CheckBoxActivity.java

---
+++ CheckBoxActivity.java
@@ -0,0 +1,14 @@
+CheckBox editCheckBox;
+editCheckBox.setChecked(false);
+editCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    
+                    
+                } else {
+                    
+                    
+                }
+            }
+        });
